0489e0a9dfaa7717d1c4e1113cac8137ff1d86c8
rltfab725@gmail.com <rltfab725@gmail.com>
2024-11-24 18:12:27 +0000

    fix : casting date error on shipped_date - orders

--- models/staging/local_bike/stg_local_bike__orders.sql
+++ models/staging/local_bike/stg_local_bike__orders.sql
@@ -2,9 +2,9 @@ select
     order_id,
     customer_id,
     order_status,
-    DATETIME(order_date, "Europe/Paris") AS order_created_at,
-    DATETIME(required_date, "Europe/Paris") AS delivery_expected_at,
-    DATETIME(shipped_date, "Europe/Paris") AS order_shipped_at,
+    order_date AS order_created_at,
+    required_date AS delivery_expected_at,
+    DATE(shipped_date) AS order_shipped_at,
     store_id, 
     staff_id
 from {{ source('local_bike', 'orders') }}
